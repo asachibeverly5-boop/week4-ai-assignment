9c029b5d51f89ef4d7b3640c2bf246379d0afab1
asachibeverly5-boop <asachibeverly5@gmail.com>
2026-03-11 12:23:50 +0300

    Add assignment.py with questions and outputs

---
+++ assignment.py
@@ -0,0 +1,33 @@
+# Question 4
+print("Hello AI Students")
+
+# Question 5
+Name="Beverly"
+Age=20
+City="Nairobi"
+print("Name:",Name)
+#Name:Beverly
+print("Age:",Age)
+#Age:20
+print("City:",City)
+#City:Nairobi
+
+#Question 6
+languages=["Python","Java","C++",JavaScript","Go"]
+print(languages[0])
+#Output-Python
+
+#Question 7
+
+student ={
+"name":"Beverly",
+"age":"21",
+"course":"AI"
+}
+print(student["name"])
+#Output-Beverly 
+
+#Bonus Question
+A List is a number of conneted items or names written or printed consecutively,typically one below the other.
+we can store multiple data but a list carries different data types.
+A Dictionary is a built-in data structure that stores data in key-value pairs.
